97e7ea013a622341963c186347d5e284c64c5fdf
tejaravutla287 <tejaravutla287@gmail.com>
2026-05-11 13:11:01 +0530

    Create DemoApplicationTests.java

---
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+ 
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+ 
+@SpringBootTest
+class DemoApplicationTests {
+ 
+@Test
+void contextLoads() {
+// This test simply checks if the Spring Application Context
+// starts up successfully. It is the most basic "smoke test."
+}
+ 
+}
